ec518329e64078677f2bf7dfdf7dcac0e1048431
tSult <sultanatasmia61@gmail.com>
2025-05-11 09:26:47 +0600

    About section done

--- src/Component/UI/HeroSection.jsx
+++ src/Component/UI/HeroSection.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className="hero-section main">
+      <div className="container grid grid-two-cols">
+        <div className="hero-content">
+          <h1 className="heading-xl">
+            Explore the world, One Country at a time.
+          </h1>
+          <p className="paragraph">
+            Discover the history, culture, and beauty of every nation. Sort,
+            search, and filter through countries to find the details you need.
+          </p>
+          <button className="btn btn-darken btn-inline bg-white-box">
+            Start Exploring <FaLongArrowAltRight />
+          </button>
+        </div>
+        <div className="hero-image">
+          <img
+            src="/public/images/world.png"
+            alt="world is beauty"
+            className="banner-image"
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import HeroSection from '../Component/UI/HeroSection'
+import About from './About'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <HeroSection/>
+    <About/>
+    </>
   )
 }
 
