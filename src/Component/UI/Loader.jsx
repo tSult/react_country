f4bb638171b1a26dec9628bd297204014c89c30c
tSult <sultanatasmia61@gmail.com>
2025-05-11 22:16:52 +0600

    completed

--- src/Component/UI/Loader.jsx
+++ src/Component/UI/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>
-      
+    <div className='container loader-section'>
+      <div className='loader'></div>
     </div>
   )
 }
--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
-
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import Loader from '../Component/UI/Loader';
+import CountryCard from '../Component/Layout/CountryCard';
+import SearchFilter from '../Component/UI/SearchFilter';
 const Country = () => {
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState("all");
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) return <Loader />;
+
+  // console.log(search, filter);
+
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
+
+  const filterRegion = (country) => {
+    if (filter === "all") return country;
+    return country.region === filter;
+  };
+
+  // here is the main logic
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+        countries={countries}
+        setCountries={setCountries}
+      />
+
+      <ul className="grid grid-four-cols">
+        {filterCountries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />;
+        })}
+      </ul>
+    </section>
+  );
+};
 
 export default Country
